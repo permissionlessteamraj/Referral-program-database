8a42c33ec9e965bf58e6f931d47830c63a37143f
permissionlessteamraj <permissionlessteam@gmail.com>
2025-04-25 10:45:11 +0530

    Update apply-referral.js

--- netlify/functions/apply-referral.js
+++ netlify/functions/apply-referral.js
@@ -3,8 +3,8 @@ const { createClient } = require('@supabase/supabase-js');
 exports.handler = async (event) => {
   const { code, userId } = JSON.parse(event.body);
   const supabase = createClient(
-    process.env.SUPABASE_URL,
-    process.env.SUPABASE_KEY
+    process.env.https://oexmwlofkkmngszkalgw.supabase.co,
+    process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9leG13bG9ma2ttbmdzemthbGd3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU1MTc5MDksImV4cCI6MjA2MTA5MzkwOX0.ib6Yr1gDL1i_M-43nWJZc1yeDZwHxklKoezC_4ZCN80
   );
 
   // Validate code
